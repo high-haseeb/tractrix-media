6919498fc5670ee6509d2532a27626932c05eaa3
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-11-11 04:08:39 +0500

    added branding

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Configurator from "@/components/Configurator";
 import Experience from "@/components/Experience";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <main className="h-screen w-screen overflow-hidden flex">
+            <Image src={"/logo.webp"} width={300} height={100} alt="logo" className="absolute top-4 left-0 z-50" />
+            <Image src={"/logo-symbol.webp"} width={150} height={100} alt="logo" className="absolute bottom-2 left-2 z-50" />
             <div className="w-[70%]"><Experience /></div>
             <div className="w-[30%]"><Configurator /></div>
         </main>
