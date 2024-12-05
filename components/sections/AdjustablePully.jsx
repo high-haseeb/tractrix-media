a3758d8a48550c3cf3d9d980e580d352ef48f7df
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-12-05 07:54:18 +0500

    max pulley and barbell = 2

--- components/sections/AdjustablePully.jsx
+++ components/sections/AdjustablePully.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 const AdjustablePulley = () => {
     return (
         <div className='flex flex-col gap-6 mt-6'>
-            <Slider />
+            <Slider max={2} />
             <Card imgSrc={() => <Image src={"/img/adjustable_pulley.webp"} width={200} height={200} alt='adjustable_pulley' className='rounded-full w-24 h-auto'/>} price={"1,000"} name={"Adjustable Pulley"} details={"15lbs-45lbs \n Set of 2"} />
             <ButtonModal title={'Feature Details'} details={''} />
         </div>
--- components/sections/Barbel.jsx
+++ components/sections/Barbel.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 const Barbell = () => {
     return (
         <div className='flex flex-col gap-6 mt-6'>
-            <Slider />
+            <Slider max={2}/>
             <Card imgSrc={() => <Image src={"/img/barbell.webp"} width={200} height={200} alt='adjustable_pulley' className='w-24 h-auto'/>} price={"500"} name={"Barbell and Weights"} details={"10bs-45lbs \n (Set of 2) +1 Barbell"} />
             <ButtonModal title={'Feature Details'} details={''} />
         </div>
