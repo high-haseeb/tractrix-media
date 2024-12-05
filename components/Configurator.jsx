9431121e7cd30d25a16d354975a400f8fb4f55f6
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-12-05 06:41:46 +0500

    removed NewFooter

--- components/Configurator.jsx
+++ components/Configurator.jsx
@@ -31,33 +31,4 @@ const Title = () => (
     <div className="font-bold lg:text-4xl text-3xl w-full text-center">Trailer Uno</div>
 );
 
-
-const NewFooter = ({ activeSectionName }) => {
-    const { nextSection, prevSection, nextSectionMobile, prevSectionMobile } = useSectionsStore();
-    const isMobile = useCheckMobile();
-
-    const ImageButton = ({ imgUrl, action }) => (
-        <button className="bg-black font-bold rounded-full shadow shadow-black/40 flex items-center justify-center p-2" onClick={action}>
-            <Image src={imgUrl} width={30} height={30} alt="navigation icon" />
-        </button>
-    );
-
-    return (
-        <div className="w-full absolute bottom-0 px-6 py-4 flex justify-between items-center">
-            <div className="flex justify-between w-full">
-                <ImageButton imgUrl="/icons/left.svg" action={isMobile ? prevSectionMobile : prevSection} />
-                {activeSectionName === "Account" ? (
-                    <ImageButton imgUrl="/icons/right.svg" action={isMobile ? nextSectionMobile : nextSection} />
-                ) : (
-                    <div className="flex justify-end w-full">
-                        <button className="w-2/3 h-10 rounded-full bg-black text-white font-semibold">
-                            Place Your Reservation
-                        </button>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
-};
-
 export default Configurator;
