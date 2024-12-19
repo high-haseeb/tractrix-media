cb413e6a838dfc080e5d6cc78eb8b138393a65a2
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-12-20 02:40:02 +0500

    removed logo from doors

--- components/Trailer.jsx
+++ components/Trailer.jsx
@@ -49,10 +49,11 @@ export function Trailer(props) {
     useLayoutEffect(() => {
         // applyProps(materials.body, { metallness: 0.5, roughness: 0.5 });
         applyProps(materials['Rubber_Rough_001_Black_50cm.001'], { roughness: 1.0, metallness: 0.0, color: "#000" })
-        applyProps(materials.D_Bumpy_Plastic, { roughness: 0.5, metallness: 0.4, color: "#222" });
+        applyProps(materials.D_Bumpy_Plastic, { roughness: 0.5, metallness: 0.8, color: "#222" });
         applyProps(materials.D_Bumpy_Plastic_2, { roughness: 1.0, metallness: 0.0, color: "#000" });
         applyProps(materials.D_Bumpy_Plastic_1, { roughness: 0.5, metallness: 0.4, color: "#222" });
-        applyProps(materials.Neon_Red, { emmisive: 200.0 });
+        applyProps(materials['speaker_side.001'], { roughness: 0.5, metallness: 0.4, color: "#222" });
+        // applyProps(materials.Neon_Red, { emmisive: 200.0 });
         woodColors.filter(color => color.name === activeWoodColor)[0].name === "choclate cork" ?
             applyProps(materials.floor_roof, { ...ashWood }) :
             applyProps(materials.floor_roof, { ...walnutWood });
@@ -101,7 +102,7 @@ export function Trailer(props) {
                 <mesh name="walktop049" castShadow receiveShadow geometry={nodes.walktop049.geometry} material={materials['C_Gun_Metal_03.001']} />
                 <mesh name="walktop049_1" castShadow receiveShadow geometry={nodes.walktop049_1.geometry} material={materials['Iron_Cast_01_1.001']} />
                 <mesh name="walktop049_2" castShadow receiveShadow geometry={nodes.walktop049_2.geometry} material={materials['Starry_Night_Paracord.001']} />
-                <mesh name="walktop049_3" castShadow receiveShadow geometry={nodes.walktop049_3.geometry} material={materials.Logo} />
+                {/* <mesh name="walktop049_3" castShadow receiveShadow geometry={nodes.walktop049_3.geometry} material={materials.Logo} /> */}
                 <mesh name="walktop049_4" castShadow receiveShadow geometry={nodes.walktop049_4.geometry} material={materials['C_Aluminum_Damaged.001']} />
                 <mesh name="walktop049_5" castShadow receiveShadow geometry={nodes.walktop049_5.geometry} material={materials['Dark_Specks_Paracord.001']} />
             </group>
@@ -111,18 +112,18 @@ export function Trailer(props) {
                 <mesh name="walktop052_2" castShadow receiveShadow geometry={nodes.walktop052_2.geometry} material={materials['Iron_Cast_01_1.001']} />
                 <mesh name="walktop052_3" castShadow receiveShadow geometry={nodes.walktop052_3.geometry} material={materials['Rubber_Grip_01_2.001']} />
                 <mesh name="walktop052_4" castShadow receiveShadow geometry={nodes.walktop052_4.geometry} material={materials['Dark_Specks_Paracord.001']} />
-                <mesh name="walktop052_5" castShadow receiveShadow geometry={nodes.walktop052_5.geometry} material={materials.Logo} />
+                {/* <mesh name="walktop052_5" castShadow receiveShadow geometry={nodes.walktop052_5.geometry} material={materials.Logo} /> */}
             </group>
             <mesh name="walktop017" castShadow receiveShadow geometry={nodes.walktop017.geometry} material={materials['C_Gun_Metal_03.001']} position={[-2.03, 2.443, -1.469]} rotation={[Math.PI / 2, 0, -1.563]} scale={0.21} />
             <group name="walktop018" position={[-2.177, 5.634, 0]} rotation={[Math.PI / 2, 1.397, 0]} scale={0.21}>
                 <mesh name="walktop084" castShadow receiveShadow geometry={nodes.walktop084.geometry} material={materials.body} />
-                <mesh name="walktop084_1" castShadow receiveShadow geometry={nodes.walktop084_1.geometry} material={materials.LogoMark} />
-                <mesh name="walktop084_2" castShadow receiveShadow geometry={nodes.walktop084_2.geometry} material={materials.Logo} />
+                {/* <mesh name="walktop084_1" castShadow receiveShadow geometry={nodes.walktop084_1.geometry} material={materials.LogoMark} /> */}
+                {/* <mesh name="walktop084_2" castShadow receiveShadow geometry={nodes.walktop084_2.geometry} material={materials.Logo} /> */}
             </group>
             <group name="walktop019" position={[2.15, 5.634, 0]} rotation={[Math.PI / 2, -1.358, 0]} scale={0.21}>
                 <mesh name="walktop056" castShadow receiveShadow geometry={nodes.walktop056.geometry} material={materials.body} />
-                <mesh name="walktop056_1" castShadow receiveShadow geometry={nodes.walktop056_1.geometry} material={materials.LogoMark} />
-                <mesh name="walktop056_2" castShadow receiveShadow geometry={nodes.walktop056_2.geometry} material={materials.Logo} />
+                {/* <mesh name="walktop056_1" castShadow receiveShadow geometry={nodes.walktop056_1.geometry} material={materials.LogoMark} /> */}
+                {/* <mesh name="walktop056_2" castShadow receiveShadow geometry={nodes.walktop056_2.geometry} material={materials.Logo} /> */}
             </group>
             <mesh name="walktop020" castShadow receiveShadow geometry={nodes.walktop020.geometry} material={materials['C_Gun_Metal_03.001']} position={[-2.03, 4.751, -1.469]} rotation={[Math.PI / 2, 0, -1.563]} scale={0.21} />
             <mesh name="walktop039" castShadow receiveShadow geometry={nodes.walktop039.geometry} material={materials['C_Gun_Metal_03.001']} position={[-2.05, 4.799, 1.231]} rotation={[Math.PI / 2, 0, 1.567]} scale={0.21} />
